fa3aa0adacc95bdd327c88ff9ab1bafdddeac305
Anis <anis.kraiem1996@gmail.com>
2022-10-12 13:58:31 +0100

    destinationServices

--- Microservices/destination-service/src/main/java/com/example/destinationservices/DestinationServicesApplication.java
+++ Microservices/destination-service/src/main/java/com/example/destinationservices/DestinationServicesApplication.java
@@ -1,13 +1,13 @@
-package com.example.destinationservice;
+package com.example.destinationservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DestinationServiceApplication {
+public class DestinationServicesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DestinationServiceApplication.class, args);
+        SpringApplication.run(DestinationServicesApplication.class, args);
     }
 
 }
--- Microservices/destination-service/src/test/java/com/example/destinationservices/DestinationServicesApplicationTests.java
+++ Microservices/destination-service/src/test/java/com/example/destinationservices/DestinationServicesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.destinationservice;
+package com.example.destinationservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DestinationServiceApplicationTests {
+class DestinationServicesApplicationTests {
 
     @Test
     void contextLoads() {
